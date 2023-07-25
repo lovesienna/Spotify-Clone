7a0fdd6c5f69d74e80cc1a5d0b64d274588dfcb3
sienna <siennahewer@gmail.com>
2023-07-25 21:32:14 +0900

    Fix: 로그아웃했는데도 여전히 플레이어 떠 있는 문제 픽스.

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,7 @@ import useAuthModal from "../hooks/useAuthModal";
 
 import Button from "./Button";
 import { useUser } from "@/hooks/useUser";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -20,6 +21,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const player = usePlayer();
   const authModal = useAuthModal();
   const router = useRouter();
 
@@ -28,7 +30,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
-    //TODO: Reset any playing songs
+    player.reset();
     router.refresh();
 
     if (error) {
